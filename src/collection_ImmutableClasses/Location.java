9f78658d34328a7e6bbed3a7ed3aeafe67c936f6
Iwona Śręba <iw.sreba@gmail.com>
2017-12-21 17:32:23 +0100

    #3. Collections - Immutable Classes

--- src/collection_ImmutableClasses/Location.java
+++ src/collection_ImmutableClasses/Location.java
@@ -1,4 +1,36 @@
 package collection_ImmutableClasses;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    //Immutable class - an immutable object is one that will not change the state after it is instantiated
+    //final fields
+    //immutable class don't have any satters
+    public Location(int locationID, String description, Map<String,Integer> exists) {
+        this.locationID = locationID;
+        this.description = description;
+//        this.exits = exists;
+        this.exits = new HashMap<String, Integer>(exists);
+        this.exits.put("Q", 0);
+    }
+//
+//    public void addExit(String direction, int location) {s
+//        exits.put(direction, location);
+//    }
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        return new HashMap<String, Integer>(exits);
+    }
 }
